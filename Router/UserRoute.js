84073ca04d8f54f4fa453df6d0df122c5cb25153
PedroMendes92 <pedro.davim.92@gmail.com>
2019-10-17 22:29:21 +0100

    add the user router

---
+++ Router/UserRoute.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var router = express.Router();
+var User = require('../models/user');
+
+function validateBody(req, res, next){
+    const {result, message} = User.validateBody(req.body, req.method);
+    if(result){
+        next()
+    }else{
+        res.status(400).send(`Invalid properties: ${message}`);
+    }
+}
+
+function isAuthenticated(req, res, next){
+    User.isAuthenticated(req.body)
+    .then(({result,message})=>{
+        if(result){
+            next()
+        }else{
+            res.status(401).send("Not logged or login has expired. Suggestion: redirect to login");
+        }
+    })
+}
+
+router.use(validateBody);
+router.use(isAuthenticated);
+
+module.exports = router;
